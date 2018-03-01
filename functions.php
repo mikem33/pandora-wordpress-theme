f7e8b4b94ebf0e92249ff995e49462d17c6da36b
Miguel Morera <info@miguelmorera.com>
2018-03-01 17:34:54 +0100

    Removed calls from the functions.php to the Modernizr and jQuery script.

--- functions.php
+++ functions.php
@@ -13,11 +13,9 @@
     function pandora_scripts(){
         // Set false if you want to load on the <head>.
         if (!is_admin()) {
-            wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr-2.6.2.min.js', null, false );
             wp_enqueue_style( 'pandora-style', get_stylesheet_uri(), array(), '1.00', 'all' );
             wp_deregister_script('jquery');
-            wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery-1.10.1.min.js', array(), null, true );
-            wp_enqueue_script( 'main-script', get_template_directory_uri() . '/assets/js/javascript.min.js', array(),'', true );
+            wp_enqueue_script( 'javascript', get_template_directory_uri() . '/assets/js/javascript.min.js', array(),'', true );
             if ( is_single() && get_option( 'thread_comments' ) ) { 
                 wp_enqueue_script( 'comment-reply' );            
             }
